982f93b1f102c88ee01741056ed9d5f3b2fb7b4b
Cornelia Scheitz <cornelia.scheitz@autodesk.com>
2019-02-24 15:03:24 -0800

    Rewrite to monitor channels and send ephemeral msgs

--- bot.py
+++ bot.py
@@ -17,54 +17,44 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
-        MESSAGE = os.environ['WELCOME_MESSAGE']
+        MESSAGE_EMP = os.environ['WELCOME_MESSAGE_EMP']
+        MESSAGE_RES = os.environ['WELCOME_MESSAGE_RES']
         TOKEN = os.environ['SLACK_TOKEN']
-        CHANNEL_TOKEN = os.environ['CHANNEL_TOKEN']
+        CHANNELS = json.loads(os.environ['CHANNELS_TO_MONITOR'])
         UNFURL = os.environ['UNFURL_LINKS']
-	RESPONSE_CHANNEL = os.environ.get('RESPONSE_CHANNEL')
-        DEBUG_CHANNEL_ID = os.environ.get('DEBUG_CHANNEL_ID', False)
 except:
-        MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
+        MESSAGE_EMP = 'Manually set the employee Message if youre not running through heroku or have not set vars in ENV'
+        MESSAGE_RES = 'Manually set the resident Message if youre not running through heroku or have not set vars in ENV'
         TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
         UNFURL = 'FALSE'
 ###############################################################
 
-def is_team_join(msg):
-    return msg['type'] == "team_join"
+def is_tc_channel_join(msg):
+    return msg['type'] == "member_joined_channel" and msg['type'] in CHANNELS and msg['channel_type'] == 'C'
 
-def is_debug_channel_join(msg):
-    return msg['type'] == "member_joined_channel" and msg['channel'] == DEBUG_CHANNEL_ID and msg['channel_type'] == 'C'
-
-def is_direct_message(msg): 
-    print msg
-    is_bot = False
-    if 'bot_id' in msg:
-        is_bot = True
-    return msg['type'] == "message" and msg['channel'][0] == 'D' and not is_bot
-
-def get_display_name(user_id):
+def get_user_info(user_id):
     logging.debug('FINDING USER WITH ID'+user_id)
-    users = requests.get("https://slack.com/api/users.list?token="+TOKEN)
-    users = users.json()
+    resp = requests.get("https://slack.com/api/users.info?token="+TOKEN+"&user="+user_id)
+    resp = resp.json()
 
-    for item in users['members']:
-        if user_id == item['id']:
-            return item['real_name']
+    user_info['real_name'] = resp['user']['real_name']
+    user_info['type'] = 'resident' if resp['user']['is_restricted'] or resp['user']['is_ultra_restricted'] else 'employee'
+    return user_info
 
 def parse_join(message):
     m = json.loads(message)
-    if is_team_join(m) or is_debug_channel_join(m):
-        user_id = m["user"]["id"] if is_team_join(m) else m["user"]
+    if is_tc_channel_join(m):
+        user_id = m["user"]
+        user_info = get_user_info(user_id)
+        channel_id = m['channel']
         logging.debug(m)
-        x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+user_id)
-        x = x.json()
-        x = x["channel"]["id"]
-        logging.debug(x)
 
+        # Different message based on user type
         data = {
                 'token': TOKEN,
-                'channel': x,
-                'text': MESSAGE,
+                'channel': channel_id,
+                'text': MESSAGE_EMP if user_info['type'] == 'employee' else MESSAGE_RES,
+                'user': user_id,
                 'parse': 'full',
                 'as_user': 'true',
                 }
@@ -74,21 +64,8 @@ def parse_join(message):
         if (UNFURL.lower() == "false"):
           data = data.update({'unfurl_link': 'false'})
 
-        xx = requests.post("https://slack.com/api/chat.postMessage", data=data)
-        logging.debug('\033[91m' + "HELLO SENT TO " + m["user"]["id"] + '\033[0m')
-
-    if is_direct_message(m):
-        logging.debug('DM RECEIVED')
-        user_id = m["user"]
-        user_message = m['text']
-        user_message = urllib.quote(user_message)
-
-        # Need to get the display name from the user_id
-        real_name = get_display_name(user_id)
-				
-        #logging.DEBUG('SENDING MESSAGE: '+user_message+' TO USER '+real_name)
-        # Need to send a message to a channel
-        requests.get("https://slack.com/api/chat.postMessage?token="+CHANNEL_TOKEN+"&channel="+RESPONSE_CHANNEL+"&text="+user_message+"&as_user=false&username="+real_name)
+        xx = requests.post("https://slack.com/api/chat.postEphemeral", data=data)
+        logging.debug('\033[91m' + "HELLO SENT TO " + user_info['real_name'] + '\033[0m')
 
 #Connects to Slacks and initiates socket handshake
 def start_rtm():
@@ -109,7 +86,7 @@ def on_close(ws):
     logging.info('\033[91m'+"Connection Closed"+'\033[0m')
 
 def on_open(ws):
-    logging.info("Connection Started - Auto Greeting new joiners to the network")
+    logging.info("Connection Started - Auto Greeting new joiners to specific channels")
 
 
 if __name__ == "__main__":
